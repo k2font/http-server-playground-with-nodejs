b4c89904a2325f983232d175ae035d646cdf9ff8
k2font <shoichiro.kouno@gmail.com>
2021-08-08 12:25:49 +0900

    eslintとprettierの導入

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+const config = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  rules: {
+    "no-else-return": "error",
+    "no-lonely-if": "error",
+    "no-self-compare": "error",
+    "no-void": "error",
+
+    "no-restricted-syntax": ["error", "TSEnumDeclaration", "TSInterfaceDeclaration"],
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+};
+
+export default config;
