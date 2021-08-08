c63cb12f717471125f7fd90cb756dfce8644e596
k2font <shoichiro.kouno@gmail.com>
2021-08-08 16:14:24 +0900

    clusterモジュールの導入

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
 import express from "express";
+import cluster from "cluster";
+import os from "os";
+const cpus = os.cpus().length;
 
-const app = express();
+// clusterモジュールの適用
+// isMasterはdeprecated
+// 推奨されるisPrimaryを指定する
+if (cluster.isPrimary) {
+  for(let i = 0; i < cpus; ++i) {
+    cluster.fork().on('listening', (address) => {
+      console.log("server listening!", address);
+    });
+  }
+} else {
+  const app = express();
 
-// GETリクエストを受け取るコード
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "ok.",
+  // GETリクエストを受け取るコード
+  app.get("/", (req, res) => {
+    res.status(200).json({
+      message: "ok.",
+    });
   });
-});
 
-app.listen(8081,() => {
-  console.log("runnning server!");
+  app.listen(8081,() => {
+    console.log("runnning server!");
+  });
+}
+
+// clusterモジュールが立てたワーカが死んだ場合
+// eventを検知して立て直す
+cluster.on("exit", (worker, code, signal) => {
+  console.log('Worker %d died with code/signal %s. Restarting worker...', worker.process.pid, signal || code);
+  cluster.fork();
 });
